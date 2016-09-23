0bdb7f68430ac11c9d76bcee9e0c97b751f13c96
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-09-23 20:56:15 -0300

    adding routes to signin and signup

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,7 @@
 var helpers = require('./helpers.js');
 
 module.exports = function(app, express){
-
+	// User routes goes here
+	app.post('/api/user/signin', userController.signin);
+	app.post('/api/user/signup', userController.signup);
 };
