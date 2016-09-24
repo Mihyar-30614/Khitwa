6845de65b10869b68b960f64d38b12dece7055ff
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-09-24 00:15:17 -0300

    modyfing routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,9 +2,11 @@ var helpers = require('./helpers.js');
 var userController = require('../users/userController.js');
 
 module.exports = function(app, express){
+	
 	// User routes goes here
-	app.post('/api/user/signin', userController.signin);
-	app.post('/api/user/signup', userController.signup);
-	app.get('/api/user/getall', userController.getAll);
-	app.get('/api/user/:username', userController.getUser)
+	app.post('/api/users/signin', userController.signin);
+	app.post('/api/users/signup', userController.signup);
+	app.get('/api/users/getall', userController.getAll);
+	app.get('/api/users/x/:username', userController.getUser);
+	app.get('/api/users/signedin', userController.checkAuth);
 };
