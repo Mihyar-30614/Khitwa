7f6e3a4864978792b9e3227646835910a08f5193
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-09-30 00:30:04 -0300

    adding organizations table

---
+++ server/organizations/organizationModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var OrganizationSchema = new mongoose.Schema({
+	EIN: String,
+  name: String,
+  causes_area: [String],
+  locations: [String],
+  missionStatement: String,
+  contactInfo : Object,
+  rate: Number,
+  picture: String,
+  currentOpportunities : [{ type: Schema.Types.ObjectId, ref: 'Opportunity' }],
+  pastOpportunities : [{ type: Schema.Types.ObjectId, ref: 'Opportunity' }],
+  owners: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+var Organization = mongoose.model('Organization' , OrganizationSchema);
+
+module.exports = Organization;
