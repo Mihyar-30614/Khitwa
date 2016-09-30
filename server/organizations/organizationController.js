ac01cf9b11c6a2c61bea801acc0c2a48ded10239
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-09-30 00:45:33 -0300

    create organization feature

---
+++ server/organizations/organizationController.js
@@ -0,0 +1,31 @@
+var organization = require('./organizationModel.js');
+var Q = require('q');
+var jwt = require('jwt-simple');
+var helpers = require('../config/helpers.js');
+
+module.exports = {
+
+	createOrganization : function (req, res) {
+		var newOrg ={
+		 EIN : req.body.EIN,
+		 name : req.body.name,
+		 causes_area : req.body.causes_area,
+		 locations : req.body.locations,
+		 missionStatement : req.body.missionStatement,
+		 contactInfo : req.body.contactInfo,
+		 rate : req.body.rate,
+		 picture : req.body.picture,
+		 currentOpportunities : req.body.currentOpportunities,
+		 pastOpportunities : req.body.pastOpportunities,
+		 owners : req.body.owners
+		};
+
+		newOrg.save( function (error, saved){
+			if (saved) {
+				res.status(201).send(JSON.stringify(saved));
+			}else{
+				helpers.errorHandler(error, req, res);
+			}
+		});
+	}
+}
