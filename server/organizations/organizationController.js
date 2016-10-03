030bcc670ffa7d2040fcab4b5fd4d0be2ce43804
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-03 20:45:47 -0300

    edit organization signup to make it unique for organization name

--- server/organizations/organizationController.js
+++ server/organizations/organizationController.js
@@ -6,25 +6,34 @@ var helpers = require('../config/helpers.js');
 module.exports = {
 
 	createOrganization : function (req, res) {
-		var newOrg = Organization({
-		 EIN : req.body.EIN,
-		 name : req.body.name,
-		 causes_area : req.body.causes_area,
-		 locations : req.body.locations,
-		 missionStatement : req.body.missionStatement,
-		 contactInfo : req.body.contactInfo,
-		 rate : req.body.rate,
-		 picture : req.body.picture,
-		 currentOpportunities : req.body.currentOpportunities,
-		 pastOpportunities : req.body.pastOpportunities,
-		 owners : req.body.owners
-		});
-
-		newOrg.save( function (error, saved){
-			if (saved) {
-				res.status(201).send(JSON.stringify(saved));
-			}else{
+		Organization.findOne({name: req.body.name})
+		.exec( function (error, found){
+			if (error) {
 				helpers.errorHandler(error, req, res);
+			} else if (found) {
+				helpers.errorHandler('Name Already Exists', req, res);
+			}else {
+				 var newOrg = Organization({
+				 EIN : req.body.EIN,
+				 name : req.body.name,
+				 causes_area : req.body.causes_area,
+				 locations : req.body.locations,
+				 missionStatement : req.body.missionStatement,
+				 contactInfo : req.body.contactInfo,
+				 rate : req.body.rate,
+				 picture : req.body.picture,
+				 currentOpportunities : req.body.currentOpportunities,
+				 pastOpportunities : req.body.pastOpportunities,
+				 owners : req.body.owners
+				});
+
+				newOrg.save( function (error, saved){
+					if (saved) {
+						res.status(201).send(JSON.stringify(saved));
+					}else{
+						helpers.errorHandler(error, req, res);
+					}
+				});
 			}
 		});
 	},
