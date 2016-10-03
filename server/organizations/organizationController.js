c168863b490a05c34ad6fe34fb6cd5b5ac9bb3c3
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-03 20:52:54 -0300

    adding a feature to edit organization

--- server/organizations/organizationController.js
+++ server/organizations/organizationController.js
@@ -53,11 +53,43 @@ module.exports = {
 		Organization.find({})
 		.exec( function (error, organization){
 			if (organization) {
-				res.status(200).send(organization);
+				res.status(200).send(JSON.stringify(organization));
 			}else if (error) {
 				helpers.errorHandler(error, req, res);
 			}else{
 				helpers.errorHandler('Organization Not Found');
+			}
+		});
+	},
+
+	editProfile :  function (req, res) {
+		Organization.findOne({ _id: req.params.is.toString()})
+		.exec( function (error, organization){
+			if (error) {
+				helpers.errorHandler(error, req, res);
+			} else if (!organization) {
+				helpers.errorHandler('Organization Not Found');
+			}else{
+
+				organization.EIN = req.body.EIN || organization.EIN;
+        		organization.name = req.body.name || organization.name;
+		        organization.causes_area = req.body.causes_area || organization.causes_area;
+		        organization.locations = req.body.locations || organization.locations;
+		        organization.missionStatement = req.body.missionStatement || organization.missionStatement;
+		        organization.contactInfo = req.body.contactInfo || organization.contactInfo;
+		        organization.rate = req.body.rate || organization.rate;
+		        organization.picture = req.body.picture || organization.picture;
+		        organization.currentOpportunities = req.body.currentOpportunities || organization.currentOpportunities;
+		        organization.pastOpportunities = req.body.pastOpportunities || organization.pastOpportunities;
+		        organization.owners = req.body.owners || organization.owners;
+
+		        organization.save(function(error, saved){
+		        	if (error) {
+		        		helpers.errorHandler(error, req, res);
+		        	}else {
+		        		res.status(201).send(JSON.stringify(saved));
+		        	}
+		        });
 			}
 		});
 	}
