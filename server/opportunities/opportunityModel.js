1c9cb71e5fbcf2392c5ee6cd85a39424242817af
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-03 21:51:57 -0300

    creating the schema for opportunities

---
+++ server/opportunities/opportunityModel.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var opportunitySchema = new Schema({
+	title : {
+		type: String, 
+		required : true
+	},
+	_organizer : {
+		type : mongoose.Schema.Types.ObjectId,
+		ref : 'Organization'
+	},
+	startDate : {
+		type : String,
+		required : true
+	},
+	endDate : String,
+	location : String,
+	causesArea : [String],
+	description : String,
+	skillsRequired : [String],
+	poster : String, 
+	currOpenings : [{type: Schema.Types.ObjectId, ref : 'Opening'}],
+	closedOpenings : [{type: Schema.Types.ObjectId, ref : 'Opening'}],
+	status : {
+		type : String,
+		enum : ['Active', 'Closed']
+	}
+});
+
+var Opportunity = mongoose.model('Opportunity', opportunitySchema);
+module.exports = Opportunity;
