9ad03ba65478668edb24ec4248a86449f148dd19
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-03 21:58:00 -0300

    Creating the intial opportunities controller

---
+++ server/opportunities/opportunityController.js
@@ -0,0 +1,10 @@
+var opportunity = require('./opportunityModel.js');
+var User = require('../users/userModel.js');
+var Q = require('q');
+var jwt = require('jwt-simple');
+var Opening = require('../openings/openingModel.js');
+var Organization = require('../organizations/organizationModel.js');
+
+module.exports = {
+	
+}
