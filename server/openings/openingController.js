b3f524017b7bccd20c8aa117dfaf032a3675a883
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-03 23:19:09 -0300

    created the essential skelton for the functions

---
+++ server/openings/openingController.js
@@ -0,0 +1,31 @@
+var Opening = require('./openingModel.js');
+var User =  require('../users/userModel.js');
+var Opportunity = require('../opportunities/opportunityModel.js');
+var Q = require('q');
+var jwt = require('jwt-simple');
+
+module.exports = {
+	allOpenings : function (req, res) {
+	},
+
+	closeOpening : function (req, res) {
+	},
+
+	deleteOpening : function (req, res) {
+	},
+
+	editOpening : function (req, res) {
+	},
+
+	applyToOpening : function (req, res) {
+	},
+
+	approveVolunteer : function (req, res) {
+	},
+
+	rejectVolunteer : function (req, res) {
+	},
+
+	getOpening : function (req, res) {
+	}
+}
