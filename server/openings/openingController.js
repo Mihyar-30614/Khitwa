39e4a1e5e182223c589c327e9247a66d44ed688a
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-08 21:08:49 -0300

    adding feature to get all openings

--- server/openings/openingController.js
+++ server/openings/openingController.js
@@ -3,9 +3,18 @@ var User =  require('../users/userModel.js');
 var Opportunity = require('../opportunities/opportunityModel.js');
 var Q = require('q');
 var jwt = require('jwt-simple');
+var helpers = require('../config/helpers.js');
 
 module.exports = {
 	allOpenings : function (req, res) {
+		Opening.find({status: "Active"})
+		.exec(function(error, openings){
+			if (openings) {
+				res.status(200).send(openings);
+			} else {
+				helpers.errorHandler(error, req, res);
+			}
+		});
 	},
 
 	closeOpening : function (req, res) {
