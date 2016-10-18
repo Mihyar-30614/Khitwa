b5e78e9cb405924b08727a7096de4b2482babae8
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-10-18 20:04:32 -0300

    Making organization standing alone entity and has its own login

--- server/organizations/organizationModel.js
+++ server/organizations/organizationModel.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var OrganizationSchema = new Schema({
   name: {type: String, required: true, unique: true},
+  pssword: {type: String, required: true}
   causes_area: [String],
   locations: [String],
   missionStatement: String,
@@ -10,9 +11,48 @@ var OrganizationSchema = new Schema({
   rate: Number,
   picture: String,
   currentOpportunities : [String],
-  pastOpportunities : [String]
+  pastOpportunities : [String],
+  salt : String
 });
 
 var Organization = mongoose.model('Organization' , OrganizationSchema);
+
+Organization.comparePassword = function(candidatePassword, savedPassword, res, cb){
+  bcrypt.compare( candidatePassword, savedPassword, function(err, isMatch){
+    if(err){
+      res.status(500).send('Error');
+    } else if(cb){
+      cb(isMatch);
+    }
+  });
+};
+
+OrganizationSchema.pre('save', function (next) {
+  var organization = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!organization.isModified('password')) {
+    return next();
+  }
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // hash the password along with our new salt
+    bcrypt.hash(organization.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      // override the cleartext password with the hashed one
+      organization.password = hash;
+      organization.salt = salt;
+      next();
+    });
+  });
+});
 
 module.exports = Organization;
