31bfe234a4782408f905740cac2317fba8539a97
Mihyar Al-Maslama <eng.mihyear@gmail.com>
2016-11-05 02:03:58 -0300

    adding tests for user sign up

--- Tests/Server/userSpec.js
+++ Tests/Server/userSpec.js
@@ -83,4 +83,69 @@ describe('User Test Database', function (done) {
 				});
 		});
 	});
-})
+
+	describe('Signing up in User Controller', function (done) {
+		
+		it('Should have a method called signup', function (done) {
+			expect(typeof userController.signup).to.be.equal('function');
+			done();
+		});
+
+		it('Should return 500 ERROR if username already exists', function (done) {
+			chai.request(server)
+				.post('/api/users/signup')
+				.send({
+					'username':'Mihyar',
+					'password':'1234',
+					'firstName':'Mihyar',
+					'lastName':'Almasalma',
+					'email':'mihyar@khitwa.org',
+					'dateOfBirth':'08-mar-1989',
+					'gender':'Male',
+					'phoneNumber':'2044055707',
+					'skills':['English','Coding'],
+					'causes':['Medical']
+				})
+				.end(function (req, res) {
+					expect(res.status).to.be.equal(500);
+					done();
+				});
+		});
+
+		it('Should signup a new user', function (done) {
+			var extraUser = new User({
+				'username':'newUser',
+				'password':'newpassword',
+				'firstName':'newbie',
+				'lastName':'user',
+				'email':'newbie@khitwa.org',
+				'dateOfBirth':'01-01-2001',
+				'gender':'Male',
+				'phoneNumber':'123456789'
+			})
+			
+			chai.request(server)
+				.post('/api/users/signup')
+				.send(extraUser)
+				.end(function (err, res) {
+					expect(err).to.be.equal(null);
+					expect(res.status).to.be.equal(201);
+					done();
+				});
+		});
+
+		it('Should response 500 ERROR if keys are not complete', function (done) {
+			var extraUser = new User({
+				'username':'newUser',
+				'password':'newpassword'
+			})
+			chai.request(server)
+				.post('/api/users/signup')
+				.send(extraUser)
+				.end(function (error, res) {
+					expect(res.status).to.be.equal(500);
+					done();
+				});
+		});
+	});
+});
