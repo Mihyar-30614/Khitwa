c310172a0640d65829cc701af8c8291f7ff3057e
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-03-12 01:16:07 -0400

    Adding Opening Services

--- www/js/services.js
+++ www/js/services.js
@@ -299,5 +299,266 @@ var link = 'http://127.0.0.1:8000';
         reset : reset
     }
 };
-.factory('Opportunity', function(){};
-.factory('Opening', function(){};
+.factory('Opportunity', function($http, $window, $location){
+    var add = function (data) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opportunity/add',
+            data : data
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return resp;
+        })
+    };
+    var close = function (id) {
+        return $http ({
+            method : 'POST',
+            url : link + '/api/opportunity/close/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var reopen = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opportunity/reopen/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    }
+    var getall = function () {
+        return $http({
+            method : 'GET',
+            url : link + '/opportunity/getall'
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var edit = function (id, data) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opportunity/edit/'+id,
+            data : data
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var currentOpenings = function (id) {
+        return $http({
+            method : 'GET',
+            url : link + '/api/opportunity/currentopenings/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var closedOpenings = function (id) {
+        return $http({
+            method : 'GET',
+            url : link + '/api/opportunity/closedopenings/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var get = function (id) {
+        return $http({
+            method : 'GET',
+            url : link + '/api/opportunity/get/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var byOrgID = function (id) {
+        return $http({
+            method : 'GET',
+            url : link + '/api/opportunity/organization/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error;
+        })
+    };
+    var remove = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opportunity/delete/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error
+        })
+    };
+
+    return {
+        add : add,
+        close : close,
+        reopen : reopen,
+        getall : getall,
+        edit : edit,
+        currentOpenings : currentOpenings,
+        closedOpenings : closedOpenings,
+        get : get,
+        byOrgID : byOrgID,
+        remove : remove
+    };
+};
+.factory('Opening', function($http, $window, $location){
+    var add = function (id, data) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/add/'+id,
+            data : data
+        })
+        .then(function (resp) {
+            return resp;
+        }).catch(function (error) {
+            return error
+        })
+    };
+    var getall = function () {
+        return $http({
+            method : 'GET',
+            url : link + '/api/opening/getall'
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var close = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/close/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var remove = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/delete/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var edit = function (id, data) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/edit/'+id,
+            data : data
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var apply = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/apply/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var approve = function (id) {
+        return $http ({
+            method : 'POST',
+            url : link + '/api/opening/approve/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var reject = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/reject/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var getone = function (id) {
+        return $http({
+            method : 'GET',
+            url : linl + '/api/opening/getOne/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    var reopen = function (id) {
+        return $http({
+            method : 'POST',
+            url : link + '/api/opening/reopen/'+id
+        })
+        .then(function (resp) {
+            return resp;
+        })
+        .catch(function (error) {
+            return error;
+        })
+    };
+    
+    return {
+        add : add,
+        getall : getall,
+        close : close,
+        remove : remove,
+        edit : edit,
+        apply : apply,
+        approve : approve,
+        reject : reject,
+        getone : getone,
+        reopen : reopen
+    };
+};
