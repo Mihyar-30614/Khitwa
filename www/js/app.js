66c4fe39bb918905b08b2fed683fffc8b77aac0e
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-03-13 14:50:56 -0300

    Adding states

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,40 @@
-// Ionic Starter App
+var app = angular.module('Khitwa', [
+    'ionic',
+    'ionic-material',
+    'Khitwa.controllers', 
+    'Khitwa.services',
+    'ui.router'
+    ]);
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    
+    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('Khitwa', ['ionic', 'Khitwa.controllers', 'Khitwa.services'])
+    $stateProvider
+    .state('/', {
+        url : '/',
+        templateUrl : 'js/templates/home.html'
+    })
+    .state('login',{
+        url : '/login',
+        templateUrl : 'js/templates/login.html',
+        controller : 'UserController'
+    })
+    .state('organizationLogin',{
+        url:'/loginOrg',
+        templateUrl : 'js/templates/loginOrg.html',
+        controller : 'OrganizationController'
+    })
 
-.run(function($ionicPlatform) {
+    $urlRouterProvider.otherwise('/');
+    $httpProvider.defaults.transformRequest = function(data) {
+        if (data === undefined) { return data; }
+        return $.param(data);
+    };
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+})
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -22,64 +49,3 @@ angular.module('Khitwa', ['ionic', 'Khitwa.controllers', 'Khitwa.services'])
     }
   });
 })
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
