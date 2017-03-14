44261465e51b285a79da5147c2059ccaa27ebf1d
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-03-14 20:25:24 -0300

    Add user signout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,6 +14,13 @@ angular.module('Khitwa.controllers', [])
 				$location.path('/');
 			}
 		})
+	};
+	$scope.signout = function () {
+		User.signout();
+		$scope.user = {};
+	}
+	$scope.signup = function () {
+		User.signup({})
 	}
 })
 
