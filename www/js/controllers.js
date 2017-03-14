e71aaa780450681b16b999109310ebe401b88579
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-03-14 19:49:37 -0300

    Fixing the development link

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,21 @@
 angular.module('Khitwa.controllers', [])
 
-.controller('UserController', function($scope) {})
+.controller('UserController', function($scope, User, $window, $location) {
+	$scope.user = {};
+	$scope.res = {};
+	$scope.signin = function () {
+		User.signin({username : $scope.user.username, password: $scope.user.password})
+		.then(function (resp) {
+			if (resp.status !== 200) {
+				$scope.res = resp;
+			} else {
+				$window.localStorage.setItem('com.khitwa',resp.data.token);
+				$window.localStorage.setItem('user',resp.data.user);
+				$location.path('/');
+			}
+		})
+	}
+})
 
 .controller('OrganizationController', function($scope) {
   // With the new view caching in Ionic, Controllers are only called
